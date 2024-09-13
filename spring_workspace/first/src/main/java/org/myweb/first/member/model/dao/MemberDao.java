c3ddfc9d46476fa7ba7359e6306f04ae0ba1c69e
GY <kgo9229@naver.com>
2024-09-13 18:15:06 +0900

    24.09.13 내 정보 보기 page 추가

---
+++ spring_workspace/first/src/main/java/org/myweb/first/member/model/dao/MemberDao.java
@@ -0,0 +1,33 @@
+package org.myweb.first.member.model.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.myweb.first.member.model.dto.Member;
+import org.myweb.first.member.model.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("memberDao")
+public class MemberDao {
+	// 쿼리문은 마이바티스 매퍼 파일에 쿼리문 별도 작성함. 
+	// root-context.xml 에 설정된 마이바티스 객체를 연결 사용함
+	@Autowired // root-context.xml 에서 생성한 객체를 자동 연결함
+	private SqlSessionTemplate sqlSessionTemplate;
+
+	// 로그인 처리용
+	public User selectLogin(User user) {
+		// 마이바티스가 제공하는 selectOne("매퍼의 엘리먼트 id명", 전달할 객체) : Object => 객체 한 개 반환됨
+		return sqlSessionTemplate.selectOne("memberMapper.selectLogin", user);
+	}
+	
+	// 회원가입 처리용
+	public int insertMember(Member member) {
+		return sqlSessionTemplate.insert("memberMapper.insertMember", member);
+	}
+	
+	// '내 정보 보기' 요청 처리용 메소드
+	public Member selectMember(String userId) {
+		return sqlSessionTemplate.selectOne("memberMapper.selectMember", userId);
+	}
+	
+}
---
+++ spring_workspace/first/src/main/java/org/myweb/first/member/model/service/MemberService.java
@@ -0,0 +1,14 @@
+package org.myweb.first.member.model.service;
+
+import org.myweb.first.member.model.dto.Member;
+import org.myweb.first.member.model.dto.User;
+
+// Spring 에서는 비즈니스 모델에 서비스 클래스는 인터페이스로 만들도록 정해져 있음.
+// 인터페이스는 추상메소드만 멤버로 가지는 추상클래스이다.
+// 작성되는 추상메소드는 기본 public abstract 반환형 메소드명(자료형 매개변수, ...);
+// public abstract 표기 생략 가능 => 상속받는 후손이 오버라이딩할 때 반드시 public 표기해야 함.
+public interface MemberService {
+	User selectLogin(User user);
+	int insertMember(Member member);
+	Member selectMember(String userid);
+}
